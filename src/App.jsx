dcb09fdd45a7a62f1a323b608e6929a995514136
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-02-04 21:24:30 +0900

    chore: 웹팩, 바벨, 리액트 세팅

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div>
+      <h1>제목</h1>
+      <p>안녕하세요. 내용입니다.</p>
+      <a href="https://www.google.com" target="_blank">
+        구글로 이동하죠
+      </a>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+  },
+};
